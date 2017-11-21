18b1247638c474374a44a4aaf9c0473d689df4e3
Konstantin Leonenko <konstantin.leo@gmail.com>
2017-11-21 14:57:35 +0000

    mapping works

--- midi2i2c.py
+++ midi2i2c.py
@@ -52,87 +52,24 @@ for p in pwm: p.set_pwm_freq(60)
 
 # note to board / channel mapper
 def note2motor(note):
-    #channel = max(0, note - 33)
-    board = (96-note) >> 4
-    channel = (96-note) % 16
+    note = max(21, note)
+    board = ((84-note) >> 4)
+    channel = (note-21) % 16
     #print 'board: ', board, 'channel: ', channel
     return board, channel
 
 
-# polytouch PWM setter
-def polytouch2vibro(note, value):
-    #board = int((63-note)/16)
-
-    board, channel = note2motor(note)
-    print 'POLYTOUCH board: ', board,'channel: ', channel, 'value: ', value*30, ' MIDI note: ', note
-    
-    pwm[board].set_pwm(channel, 0, value*30)
-
-# note OFF
-def motor_off(note):
-    board, channel = note2motor(note)
-    #board = int((63-note)/16)
-    print 'NOTE OFF board: ', board, 'channel: ', channel, ' MIDI note: ', note
-    pwm[board].set_pwm(note%16, 0, 0)
-
 # MIDI listener
 with mido.open_input('Xkey:Xkey MIDI 1 20:0') as inport:
     for msg in inport:
+        board, channel = note2motor(msg.note)
+        
         print msg
+        print 'board: ', board, 'channel: ', channel
+        
         if msg.type == 'polytouch':
-            #pwm[0].set_pwm(1,0,msg.value*30)
-            polytouch2vibro(msg.note,msg.value)
-            note2motor(msg.note)
+            pwm[board].set_pwm(channel, 0, msg.value*30)
 
         if msg.type == 'note_off':
-            #print 'off'
-            motor_off(msg.note)
+            pwm[board].set_pwm(channel, 0, 0)
 
-     
-
-
-#
-# OSC
-#
-
-
-# SETTING UP OSC Server and message handlers
-#server = OSCServer (("192.168.0.101",57120))
-#client = OSCClient()
-
-#def handle_timeout(self):
-#    print("I'm IDLE")
-
-#server.handle_timeout = types.MethodType(handle_timeout, server)
-
-# fader handlers
-
-#def fader_callback(path, tags, args, source):
-#    multifader = path.split("/")[2]
-    
-#    fader_osc = int(path.split("/")[3]) - 1
-    
-#    pwm_value = int(args[0]*(servo_max-servo_min)+servo_min)
-    
-#    fader_i2c = [fader_osc >> 4, fader_osc % 16, pwm_value];
-    
-#    if multifader == "multifader2":
-#        fader_i2c[0] += 2
-
-#   pwm[fader_i2c[0]].set_pwm(fader_i2c[1],0,fader_i2c[2])
-    
-#    print "board: ", fader_i2c[0], ", fader: ", fader_i2c[1], ", value: ", fader_i2c[2]
-
-
-
-
-# OSC message handlers
-#for i in range(1,33):
-#    server.addMsgHandler( "/1/multifader1/"+str(i), fader_callback)
-#    server.addMsgHandler( "/1/multifader2/"+str(i), fader_callback)
-
-
-#while True:
-#    server.handle_request()
-
-#server.close()
