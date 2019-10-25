3f9b8babbd05a3833fd9a1e646c4df76cdef2b79
s8 <konstantin.leo@gmail.com>
2019-10-25 11:38:04 +0000

    work in progress backup

--- max2i2c.py
+++ max2i2c.py
@@ -47,11 +47,11 @@ for p in pwm:
     p.set_pwm_freq(60)
 
 # SETTING UP OSC Server and message handlers
-# server = OSCServer (("192.168.1.2",57120))
 # this has to be the address of Pi itself
 # weirdly enough two sets of brackets are needed
 # the port here has to match "outgoing" port on the controller app
-server = OSCServer(("192.168.1.6",8000))
+#server = OSCServer(("192.168.1.6",8000))
+server = OSCServer(("192.168.1.4",8000))
 client = OSCClient()
 
 def handle_timeout(self):
@@ -62,32 +62,19 @@ server.handle_timeout = types.MethodType(handle_timeout, server)
 # fader handlers
 
 def fader_callback(path, tags, args, source):
+    #print args
 
-    # store the name of the TouchOSC interface element
-    multifader = path.split("/")[2]
-    
-    fader_osc = int(path.split("/")[3]) - 1
-    
-    pwm_value = int(args[0]*(servo_max-servo_min)+servo_min)
-    
-    board_i2c = [fader_osc >> 4, fader_osc % 16, pwm_value];
-    
-    #if multifader == "multifader2":
-    #    board_i2c[0] += 2
+    board = args[0]>>4
+ #   board = 3
+    motor = args[0]%16
+ #   motor = 3
+    value = int(args[1]*(servo_max-servo_min)+servo_min)
 
-    pwm[board_i2c[0]].set_pwm(board_i2c[1],0,board_i2c[2])
-    
-    print "board: ", board_i2c[0], ", fader: ", board_i2c[1], ", value: ", board_i2c[2]
-
-# origignal layout with 2 x32 multifaders
-#for i in range(1,33):
-#    server.addMsgHandler( "/1/multifader1/"+str(i), fader_callback)
-#    server.addMsgHandler( "/1/multifader2/"+str(i), fader_callback)
+    pwm[board].set_pwm(motor,0,value)
 
 for i in range(0,63):
-    print 'registering callback for fader: ', i
-    server.addMsgHandler( "/multifader/multifader/"+str(i), fader_callback)
-    #server.addMsgHandler( "/1/multifader2/"+str(i), fader_callback)
+    #print 'registering callback for fader: ', i
+    server.addMsgHandler( "/multifader/", fader_callback)
 
 while True:
     server.handle_request()
