ce892abfbabaa73ab9ad646a418f1095d3ed1b26
s8 <konstantin.leo@gmail.com>
2019-10-25 17:57:38 +0100

    WIP debugging mapping

--- osc2servo.py
+++ osc2servo.py
@@ -69,7 +69,7 @@ def fader_callback(path, tags, args, source):
 
     motor = int(path.split("/")[2])
     board = 5 - int(motor / 16)
-    channel = 15 - int(motor % 16)
+    channel = int(motor % 16)
 
     # board = (int(path.split("/")[3]) - 1) >> 4
     # motor = 16 - ((int(path.split("/")[3]) - 1) % 16)
